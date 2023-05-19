a5b7fa88b09074f2abba5d4de2b870ad0763abc3
Mahbub Ali <mahbubaligub192@gmail.com>
2023-05-19 10:36:58 +0600

    get data find by id

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ async function run() {
       res.send(toys);
     });
 
+    //get toy find by ID
+    app.get("/findToyById/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allToyCollection.find(query).toArray();
+      res.send(result);
+    });
+
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
