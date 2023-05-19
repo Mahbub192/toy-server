099821d64a4a4c3cbf159e4768d2f62bddbe73e6
Mahbub Ali <mahbubaligub192@gmail.com>
2023-05-19 11:01:01 +0600

    update a singlee toy

--- index.js
+++ index.js
@@ -84,7 +84,18 @@ async function run() {
       res.send(result)
     })
 
-  
+    //update the toy
+    app.put("/updateToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+      };
+      const result = await allToyCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
